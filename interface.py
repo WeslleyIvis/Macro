cadb749a525643729e211248dde2407837a6dc28
WeslleyIvis <weslley_reive@hotmail.com>
2022-06-18 18:17:24 -0300

    Script funcionando sem travar a interface

--- interface.py
+++ interface.py
@@ -8,17 +8,35 @@ sg.theme('Dark Grey 13')
 
 layout_hotkey = [
   [sg.Push(), sg.Text('Hotkey'), sg.Push()],
-  [sg.Text('Hotkey 1'), sg.Input(key='-HOTKEY_1-', s=10, default_text='A')],
+  [sg.Text('Hotkey 1'), sg.Input(key='-HOTKEY_1-',
+      s=10, 
+      default_text='A')],
+  [sg.Text('Hotkey 2'), sg.Input(key='-HOTKEY_2-',
+      s=10, 
+      default_text='7')],
+  [sg.Text('Hotkey 3'), sg.Input(key='-HOTKEY_3-',
+      s=10, 
+      default_text='Ctrl 2')],
 ]
 
 layout_time = [
   [sg.Push(), sg.Text('Hotkey deley '), sg.Push()],
-  [sg.Text('Time'), sg.Input(key='-HOTKEY_TIME_1-',s=10, default_text=5)],
+  [sg.Text('Time'), sg.Input(key='-HOTKEY_TIME_1-',
+     s=10, 
+     default_text=5)],
+  [sg.Text('Time'), sg.Input(key='-HOTKEY_TIME_2-',
+     s=10, 
+     default_text=5)],
+  [sg.Text('Time'), sg.Input(key='-HOTKEY_TIME_3-',
+     s=10, 
+     default_text=5)],
 ]
 
 layout_time_start = [
   [sg.Push(), sg.Text('Start deley'), sg.Push()],
-  [sg.Input(key='-TIME_START-', s=10, default_text=5)],
+  [sg.Input(key='-TIME_START-',
+   s=10,
+   default_text=5)],
 ]
 
 layout = [
@@ -37,41 +55,65 @@ window = sg.Window(
 global starter 
 starter = False
 
-def operation_thread(seconds, window):
+def operation_thread(seconds, hotkey, window):
   print('Starting thread - will sleep for {} seconds'.format(deley_start))
-  sleep(deley_start)
-  print('Go!')
   count = 0
+  sleep(deley_start)
+
+  print('Go!')
   while starter:
     if starter == False:
+      break
+    elif seconds < 1:
+      print(f'{hotkey}: OFF')
       break
     sleep(seconds)   
     count += 1
     print('Hotkey: ', hotkey, ' -- Deley: ', seconds_hotkey, '-- Count: ', count)
-    press_hotkey()
-
+    press_hotkey(hotkey)
   print('-THREAD-', '** DONE **')
 
-def press_hotkey():
-  if len(hotkey) >= 2:
-    pyautogui.hotkey(hotkey[0], hotkey[1])
-  else: pyautogui.hotkey(hotkey[0])
+def press_hotkey(hotkey_button):
+  if len(hotkey_button) >= 2:
+    pyautogui.hotkey(hotkey_button[0], hotkey_button[1])
+  else: pyautogui.hotkey(hotkey_button[0])
+
+def valid_value(value):
+  if type(value) != int:
+    if value.isnumeric():
+      value = int(value)
+    else: 
+      sg.popup(f'Invalid value: {value}  -- APENAS NUMEROS--')
+  return value
 
 while True:
   event, values = window.read()
-  #print(event, values) 
-      
-  string_hotkey = values["-HOTKEY_1-"]
-  hotkey = string_hotkey.split(' ')
-  seconds_hotkey = int(values["-HOTKEY_TIME_1-"])   
-  deley_start = int(values["-TIME_START-"])
+  print(event, values) 
 
-  if event == 'Start':
+  deley_start = valid_value(values["-TIME_START-"])
+  seconds_hotkey = {
+    'value_1': valid_value(values["-HOTKEY_TIME_1-"]),
+    'value_2': valid_value(values["-HOTKEY_TIME_2-"]),
+    'value_3': valid_value(values["-HOTKEY_TIME_3-"])
+    }
+  hotkey = {
+    'hotkey_1': values["-HOTKEY_1-"].split(' '),
+    'hotkey_2': values["-HOTKEY_2-"].split(' '),
+    'hotkey_3': values["-HOTKEY_3-"].split(' '),
+    }
+
+  if event == 'Start' and type(seconds_hotkey['value_1']) == int and type(deley_start) == int:
     starter = True
-  else: starter = False
+  else:
+     starter = False
+     deley_start = 0
+     seconds_hotkey = 0
 
   if event in (sg.WIN_CLOSED, 'Exit'):
     break
-  elif event.startswith('Start'):
+  elif event.startswith('Start') and starter == True:
     print('Thread alive! value: {}'.format(seconds_hotkey))
-    threading.Thread(target=operation_thread, args=(seconds_hotkey, window), daemon=True).start()
+    threading.Thread(target=operation_thread, args=(seconds_hotkey['value_1'], hotkey['hotkey_1'], window), daemon=True).start()
+    threading.Thread(target=operation_thread, args=(seconds_hotkey['value_2'], hotkey['hotkey_2'], window), daemon=True).start()
+    threading.Thread(target=operation_thread, args=(seconds_hotkey['value_3'], hotkey['hotkey_3'], window), daemon=True).start()
+
