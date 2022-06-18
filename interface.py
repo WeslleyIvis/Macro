f1e32f93ea543d21177d2793a9100dc794f0d570
WeslleyIvis <weslley_reive@hotmail.com>
2022-06-18 05:17:49 -0300

    interface funcionando com thread

--- interface.py
+++ interface.py
@@ -1,69 +1,29 @@
 import PySimpleGUI as sg
-#import script as Macro
+import threading
 from time import sleep
-from random import randint
 import pyautogui
+from random import randint
 
-class Macro:
-  def __init__(self,value_start,stop,hotkey,time_hotkey):
-    self.value_start = int(value_start)
-    self.stop = int(stop)
-    self.hotkey = hotkey
-    self.time_hotkey = time_hotkey
-    self.COUNT_TIME = []
-    self.COUNT_STOP = []
-
-  def event(self, value:bool):
-    while value:
-      #print('start')
-      sleep(self.value_start)
-      self.value_start = self.rand_time(self.time_hotkey)
-      self.append_time()
-      self.start_hotkey()
-
-      if sum(self.COUNT_STOP) >= self.stop:
-        break
-
-  def append_time(self):
-    self.COUNT_TIME.append(self.value_start)
-    self.COUNT_STOP.append(self.value_start)
-
-  def start_hotkey(self):
-    if sum(self.COUNT_TIME) >= self.time_hotkey:
-      pyautogui.hotkey(self.hotkey)
-    self.COUNT_TIME.clear()
-
-  def rand_time(self, value:int):
-    value += randint(-10, 10)
-    return value
-
-  def init(self, true_false:bool):
-    self.event(true_false)
-
-sg.theme('darkPurple2')
+sg.theme('Dark Grey 13')
 
 layout_hotkey = [
-  [sg.Push(), sg.Text('HOTKEYS'), sg.Push()],
-  [sg.Text('Hotkey 1'), sg.Input(key='-HOTKEY_1-', s=10)],
-  [sg.Text('Hotkey 2'), sg.Input(key='-HOTKEY_2-', s=10)],
-  [sg.Text('Hotkey 3'), sg.Input(key='-HOTKEY_3-', s=10)],
+  [sg.Push(), sg.Text('Hotkey'), sg.Push()],
+  [sg.Text('Hotkey 1'), sg.Input(key='-HOTKEY_1-', s=10, default_text='A')],
 ]
 
 layout_time = [
-  [sg.Push(), sg.Text('TIME ACTIONS'), sg.Push()],
-  [sg.Text('Time'), sg.Input(key='-HOTKEY_TIME_1-',s=10)],
-  [sg.Text('Time'), sg.Input(key='-HOTKEY_TIME_2-',s=10)],
-  [sg.Text('Time'), sg.Input(key='-HOTKEY_TIME_3-',s=10)],
+  [sg.Push(), sg.Text('Hotkey deley '), sg.Push()],
+  [sg.Text('Time'), sg.Input(key='-HOTKEY_TIME_1-',s=10, default_text=5)],
 ]
 
 layout_time_start = [
-  [sg.Push(), sg.Text('START IN'), sg.Push()],
-  [sg.Input(key='-TIME_START-', s=10)],
-  [sg.Push(), sg.Text('STOP IN'), sg.Push()],
-  [sg.Input(key='-TIME_STOP-', s=10)]
+  [sg.Push(), sg.Text('Start deley'), sg.Push()],
+  [sg.Input(key='-TIME_START-', s=10, default_text=5)],
 ]
 
 layout = [
+  [sg.StatusBar('StatusBar', s=10), sg.StatusBar('Threds: ',)],
+  [sg.Output(size=(70,12))],
   [sg.Column(layout_hotkey), sg.VSeparator(), sg.Column(layout_time), sg.VSeparator(), sg.Column(layout_time_start)],
   [sg.Button('Start', p=10), sg.Button('Stop')]
 ]
@@ -74,32 +34,44 @@ window = sg.Window(
   element_justification='c',
 )
 
-START = False
+global starter 
+starter = False
+
+def operation_thread(seconds, window):
+  print('Starting thread - will sleep for {} seconds'.format(deley_start))
+  sleep(deley_start)
+  print('Go!')
+  count = 0
+  while starter:
+    if starter == False:
+      break
+    sleep(seconds)   
+    count += 1
+    print('Hotkey: ', hotkey, ' -- Deley: ', seconds_hotkey, '-- Count: ', count)
+    press_hotkey()
+
+  print('-THREAD-', '** DONE **')
+
+def press_hotkey():
+  if len(hotkey) >= 2:
+    pyautogui.hotkey(hotkey[0], hotkey[1])
+  else: pyautogui.hotkey(hotkey[0])
 
 while True:
   event, values = window.read()
-  print(event, values) 
+  #print(event, values) 
+      
+  string_hotkey = values["-HOTKEY_1-"]
+  hotkey = string_hotkey.split(' ')
+  seconds_hotkey = int(values["-HOTKEY_TIME_1-"])   
+  deley_start = int(values["-TIME_START-"])
 
   if event == 'Start':
-    START = True
-  else: START = False
+    starter = True
+  else: starter = False
 
-  #HOTEY_1 = Macro()
-  
-  if START:
-    HOTEY_1 = Macro(value_start = values["-TIME_START-"],
-     stop = values["-HOTKEY_1-"], 
-     hotkey = values["-HOTKEY_TIME_1-"],
-     time_hotkey = int(values["-TIME_STOP-"])
-    )
-    
-
-    print(values["-TIME_START-"])
-
-    HOTEY_1.event(START)
-
-  
-  print(HOTEY_1.rand_time(20))
-
-  if values == None or event == sg.WIN_CLOSED:
+  if event in (sg.WIN_CLOSED, 'Exit'):
     break
+  elif event.startswith('Start'):
+    print('Thread alive! value: {}'.format(seconds_hotkey))
+    threading.Thread(target=operation_thread, args=(seconds_hotkey, window), daemon=True).start()
--- test.py
+++ test.py
@@ -1,42 +1,99 @@
-from ast import Break
-from PySimpleGUI import (Window, Button, Text, Image, Input, Column, VSeparator, Push, theme, popup)
+# from ast import Break
+# from PySimpleGUI import (Window, Button, Text, Image, Input, Column, VSeparator, Push, theme, popup)
 
-theme('darkPurple2')
+# theme('darkPurple2')
 
-layout_direita = [
-  [Text('E-mail'), Input(key='-EMAIL-')],
-  [Text('Senha'), Input(password_char='*', key='-SENHA-')],
-  [Push(), Button('Login'), Push(), Button('Esqueci a Senha'), Push()]
-]
+# layout_direita = [
+#   [Text('E-mail'), Input(key='-EMAIL-')],
+#   [Text('Senha'), Input(password_char='*', key='-SENHA-')],
+#   [Push(), Button('Login'), Push(), Button('Esqueci a Senha'), Push()]
+# ]
 
-layout_esquerda = [
-  [Image(filename='Human.png', key='-IMAGE-')]
-]
+# layout_esquerda = [
+#   [Image(filename='Human.png', key='-IMAGE-')]
+# ]
 
-layout = [
-  [Image(filename='simpe.jpg')],
-  [Column(layout_esquerda), VSeparator(), Column(layout_direita)]
-]
+# layout = [
+#   [Image(filename='simpe.jpg')],
+#   [Column(layout_esquerda), VSeparator(), Column(layout_direita)]
+# ]
 
-window = Window(
-  'Macro Ninfox',
-  layout = layout,
-  #element_justification='c'
-)
+# window = Window(
+#   'Macro Ninfox',
+#   layout = layout,
+#   #element_justification='c'
+# )
 
-while True:
-  event, values = window.read()
-  print(event,' - ', values)
+# while True:
+#   event, values = window.read()
+#   print(event,' - ', values)
 
-  def interface(value):
-    if value == 'Login':
-      print('Butao de login')
-    if value == 'Esqueci a Senha':
-      popup(f'Seu email é {values["-EMAIL-"]}')
+#   def interface(value):
+#     if value == 'Login':
+#       print('Butao de login')
+#     if value == 'Esqueci a Senha':
+#       popup(f'Seu email é {values["-EMAIL-"]}')
 
-  interface(event)
+#   interface(event)
   
-  if values == None:
-    break  
+#   if values == None:
+#     break  
 
   
+
+import threading
+import time
+import PySimpleGUI as sg
+
+
+
+def long_operation_thread(seconds, window):
+    """
+    A worker thread that communicates with the GUI through a queue
+    This thread can block for as long as it wants and the GUI will not be affected
+    :param seconds: (int) How long to sleep, the ultimate blocking call
+    :param gui_queue: (queue.Queue) Queue to communicate back to GUI that task is completed
+    :return:
+    """
+    print('Starting thread - will sleep for {} seconds'.format(seconds))
+    time.sleep(seconds)                  # sleep for a while
+    window.write_event_value('-THREAD-', '** DONE **')  # put a message into queue for GUI
+
+
+def the_gui():
+    """
+    Starts and executes the GUI
+    Reads data from a Queue and displays the data to the window
+    Returns when the user exits / closes the window
+    """
+    sg.theme('Light Brown 3')
+
+    layout = [[sg.Text('Long task to perform example')],
+              [sg.Output(size=(70, 12))],
+              [sg.Text('Number of seconds your task will take'),
+                  sg.Input(key='-SECONDS-', size=(5, 1)),
+                  sg.Button('Do Long Task', bind_return_key=True)],
+              [sg.Button('Click Me'), sg.Button('Exit')], ]
+
+    window = sg.Window('Multithreaded Window', layout)
+
+    # --------------------- EVENT LOOP ---------------------
+    while True:
+        event, values = window.read()
+        if event in (sg.WIN_CLOSED, 'Exit'):
+            break
+        elif event.startswith('Do'):
+            seconds = int(values['-SECONDS-'])
+            print('Thread ALIVE! Long work....sending value of {} seconds'.format(seconds))
+            threading.Thread(target=long_operation_thread, args=(seconds, window,), daemon=True).start()
+        elif event == 'Click Me':
+            print('Your GUI is alive and well')
+        elif event == '-THREAD-':
+            print('Got a message back from the thread: ', values[event])
+
+    # if user exits the window, then close the window and exit the GUI func
+    window.close()
+
+if __name__ == '__main__':
+    the_gui()
+    print('Exiting Program')
